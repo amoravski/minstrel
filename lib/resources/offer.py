03e7315e9ea7c3fed557f34d1dd9795f2e86f30f
alex <alex@guardium.eu>
2018-12-06 17:20:35 +0200

    Moved parsers to parsers file

--- lib/resources/offer.py
+++ lib/resources/offer.py
@@ -1,17 +1,12 @@
 from flask_restful import Resource, reqparse
 from flask_jwt_extended import get_jwt_identity, jwt_required, get_jwt_claims, fresh_jwt_required, jwt_optional
 from models.event import OfferModel
+from resources.parsers import offer_parser
 
 class Offer(Resource):
     """
         Methods by which events are made possible
     """
-    parser = reqparse.RequestParser()
-    parser.add_argument('text',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
 
     def get(self, title):
         offer = OfferModel.find_by_title(title)
@@ -24,7 +19,7 @@ class Offer(Resource):
         if OfferModel.find_by_title(title):
             return {'message': "An Offer with name '{}' already exists.".format(title)}, 400
 
-        data = self.parser.parse_args()
+        data = offer_parser.parse_args()
 
         user_id = get_jwt_identity()
         offer = OfferModel(title, data['text'], user_id)
