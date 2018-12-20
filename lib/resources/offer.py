3a1d5dacda1b2a3a9ce3b764a25f1b50f3c87402
alex <alexmoravski@gmail.com>
2018-12-20 19:15:36 +0200

    Updated offer creation, standartized returns

--- lib/resources/offer.py
+++ lib/resources/offer.py
@@ -2,6 +2,8 @@ from flask_restful import Resource, reqparse
 from flask_jwt_extended import get_jwt_identity, jwt_required, get_jwt_claims, fresh_jwt_required, jwt_optional
 from models.event import OfferModel
 from resources.parsers import offer_parser
+from models.user import PerformerModel, AdmirerModel
+from time import time
 
 class Offer(Resource):
     """
@@ -12,22 +14,46 @@ class Offer(Resource):
         offer = OfferModel.find_by_title(title)
         if offer:
             return offer.json()
-        return {'message': 'Offer not found'}, 404
+        return {'status': 'error','message': 'Offer not found'}, 404
 
     @fresh_jwt_required
     def post(self, title):
+        user_id = get_jwt_identity()
+        if not AdmirerModel.find_by_email(user_id):
+            return {'status': 'error', 'message': 'Only admirers can make offers'}, 400
+
         if OfferModel.find_by_title(title):
-            return {'message': "An Offer with name '{}' already exists.".format(title)}, 400
+            return {'status': 'error','message': 'An offer with name {} already exists.'.format(title)}, 400
 
         data = offer_parser.parse_args()
-
-        user_id = get_jwt_identity()
-        offer = OfferModel(title, data['text'], user_id)
+        
+        try:
+            accepted_categories = PerformerModel.filter_categories(data['categories'])
+            if accepted_categories['status'] == "error":
+                return accepted_categories
+        except Exception as e:
+            return {'status':'error', 'message': 'Something went wrong when processing parameters' + str(e)}, 500
+        
+        if not int(data['date'])>int(time()):
+            return {'status': 'error', 'message': 'Date must be in the future'}, 400
+        
+        offer = OfferModel(
+                title, 
+                data['text'], 
+                user_id, 
+                data['location'], 
+                data['date'], 
+                accepted_categories['categories'],
+                data['size'],
+                data['type'],
+                data['requirements'],
+                data['compensation'],
+                )
 
         try:
             offer.save_to_db()
-        except:
-            return {"message": "An error occurred while creating the offer."}, 500
+        except Exception as e:
+            return {'status': 'error', 'message': 'Something went wrong when processing parameters'}, 500
 
         return offer.json(), 201
 
